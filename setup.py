7e28bbf2294a3523b8dbaa754dd30430e149f891
Jeffrey Mullins <jmullins@30and30.com>
2012-03-29 11:55:33 -0400

    Initial Tech Residents Services Core library skeleton.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+import os
+
+def find_packages():
+    packages = []
+    for dir,subdirs,files in os.walk('trsvcscore'):
+        package = dir.replace(os.path.sep, '.')
+        if '__init__.py' not in files:
+            # not a package
+            continue
+        packages.append(package)
+    return packages
+
+setup(
+    name='trsvcscore',
+    version = '0.1-SNAPSHOT',
+    author = '30and30',
+    packages = find_packages(),
+    license = 'LICENSE',
+    description = '30and30 Python Tech Residents Services Core Library',
+    long_description = open('README').read(),
+)
---
+++ trsvccore/__init__.py
@@ -0,0 +1,3 @@
+"""30and30 Python Tech Residents Services Core Package"""
+
+__all__ = []
