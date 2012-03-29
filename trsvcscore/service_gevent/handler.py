117d356b3f1c37c502d08e3c46a17c440cae8991
Jeffrey Mullins <jmullins@30and30.com>
2012-03-29 19:51:28 -0400

    Adding service_common as a placeholder for code common to both service and service_gevent.

--- trsvcscore/service_gevent/handler.py
+++ trsvcscore/service_gevent/handler.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import logging
+
+from trpycore.mongrel2_common.request import SafeRequest
+
 from tridlcore.gen import TRService
 from tridlcore.gen.ttypes import ServiceStatus
 
@@ -54,9 +58,41 @@ class ServiceHandler(TRService.Iface, object):
         pass
 
 class Mongrel2Handler(ServiceHandler):
-    """Base class for Mongrel2 service handler"""
+    """Base class for Mongrel2 service handler
+       
+       Incoming http requests will be delegated to a method named as follows:
+
+          handle_<method>_<url_with_slashes_replaced_by_underscores>
+          
+          where method is "get", "post", etc.. and <url...> is the full
+          url with the slashes replaced by underscores.
+
+      For example a POST to www.server.com/myapp/chat will invoke
+      the "handle_post_myapp_chat" method with a SafeRequest as 
+      the sole parameter.
+    """
+
     def __init__(self, *args, **kwargs):
         super(Mongrel2Handler, self).__init__(*args, **kwargs)
     
-    def handle(self, connection, request):
-        pass
+    def handle(self, connection, unsafe_request):
+        if unsafe_request.is_disconnect():
+            return
+
+        request = SafeRequest(unsafe_request)
+        url = request.header("PATH")
+        method = request.method()
+
+        try:
+            handler_name = "handle_%s%s" % (method, url.replace("/", "_"))
+            handler = getattr(self, handler_name.lower())
+            response = handler(request)
+            connection.reply_http(unsafe_request, response)
+
+        except AttributeError as error:
+            logging.exception(error)
+            connection.reply_http(unsafe_request, "not found", code=404)
+        
+        except Exception as error:
+            logging.exception(error)
+            connection.reply_http(unsafe_request, "internal error", code=503)
