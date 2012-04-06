34038c71e61509aa18dbd65c79e175c6d57bad81
Jeffrey Mullins <jmullins@30and30.com>
2012-04-06 16:13:29 -0400

    Adding support for kwargs to mongrel2 session decorator.

--- trsvcscore/decorators/mongrel2.py
+++ trsvcscore/decorators/mongrel2.py
@@ -6,15 +6,16 @@ def session_required(func):
     Checks to see if the incoming Mongrel2 request was sent with a valid sessionid cookie.
     This decorator needs a SessionStore object available in self.session_store to run.
     If the session is valid the decorated method will be invoked with an additional
-    session argument containing the validate session.
+    session argument, immediately following the request paramater, containing the validate session.
+    All additoinal keyword arguments will follow the session argument.
 
     Raises:
         HTTPError if valid session is not found.
     """
-    def check(self, request):
+    def check(self, request, **kwargs):
         session = self.session_store.get_session(request.cookie("sessionid"))
         if not session:
             raise HttpError(401, "access denied")
         else:
-            return func(self, request, session)
+            return func(self, request, session, **kwargs)
     return check
