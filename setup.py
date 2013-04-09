f2c80456236dfe8de18e5122ec9396fff4d3e713
Jeffrey Mullins <jmullins@techresidents.com>
2013-04-09 11:07:54 -0400

    Bumping version to 0.22-SNAPSHOT

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def find_packages():
 
 setup(
     name='trsvcscore',
-    version = '0.21.0',
+    version = '0.22-SNAPSHOT',
     author = 'Tech Residents, Inc.',
     packages = find_packages(),
     license = open('LICENSE').read(),
