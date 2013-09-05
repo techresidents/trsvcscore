7e58ec11ced6c97fbf89c4a528e04c563985833e
Jeffrey Mullins <jmullins@techresidents.com>
2013-09-05 12:08:41 -0400

    Updating dependencies.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         ],
     install_requires=[
         'trpycore>=0.11.0',
-        'trhttp>=0.2.0',
-        'trrackspace>=0.1.0',
+        'trhttp>=0.4.0',
+        'trrackspace>=0.2.0',
     ]
 )
