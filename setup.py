f3c091e3b5a7e312b5e4447d54240d24190ebe9a
Jeffrey Mullins <jmullins@techresidents.com>
2013-10-09 11:45:57 -0400

    Bumping version to 0.36-SNAPSHOT

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ ez_setup.use_setuptools()
 
 setup(
     name='trsvcscore',
-    version = '0.35.0',
+    version = '0.36-SNAPSHOT',
     author = 'Tech Residents, Inc.',
     packages = find_packages(),
     license = open('LICENSE').read(),
